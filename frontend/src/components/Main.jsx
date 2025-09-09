487a2ae8a8cfe2992c464250bb8445130e910126
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-09 10:13:45 +0200

    add form state

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,16 +1,38 @@
-import "../style/Main.css"
+import "../style/Main.css";
+import { useState } from "react";
 
 export const Main = () => {
-    return(
+    const [ingredients, setIngredient] = useState([]);
+    
+    const addIngredient = (formData) => {
+        const newIngredient = formData.get("ingredient");
+        
+        setIngredient((prevIngr) => [
+            ...prevIngr,
+            newIngredient
+        ]);
+        
+    };
+    
+    return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
-                    aria-label="Add ingredient" 
-                    type="text" 
+            <form
+                className="add-ingredient-form"
+                action={addIngredient}
+            >
+                <input
+                    aria-label="Add ingredient"
+                    type="text"
                     placeholder="e.g egg"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
         </main>
-    )
-}
+    );
+};
