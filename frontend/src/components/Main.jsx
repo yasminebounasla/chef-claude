a98600ddf4f9c52a241e0afeae1d5f64bd42d386
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-10 09:05:39 +0200

    add some UI

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export const Main = () => {
     const [ingredients, setIngredient] = useState([]);
+    const [recipeShown, setRecipeShown] =useState(false);
     
     const addIngredient = (formData) => {
         const newIngredient = formData.get("ingredient");
@@ -13,6 +14,10 @@ export const Main = () => {
         ]);
         
     };
+
+    const toggleRecipeShwon = ()=> {
+        setRecipeShown( prev => !prev )
+    }
     
     return (
         <main>
@@ -28,11 +33,44 @@ export const Main = () => {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                ))}
-            </ul>
+
+            { ingredients.length > 0 && (
+                <section>
+                <h1>Ingredients on hand :</h1>
+                <ul>
+                    {ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    ))}
+                </ul>
+
+                {ingredients.length > 3 && (
+                    <div className="get-recipe-container">
+                        <div>
+                            <h3>Ready for a recipe ?</h3>
+                            <p>Generate a recipr from your list of ingredients</p>
+                        </div>
+
+                        <button onClick={toggleRecipeShwon}>Get a recipe</button>
+                    </div> )}
+            </section> )}
+
+
+            {recipeShown && (
+                <section>
+                    <h2>Chef Claude Recommends:</h2>
+                    <p>Based on the ingredients you have available, I would recommend {}. Here is the recipe:</p>
+
+                    <h3>Beef Bolognese Pasta</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
+
+                    <h3>Instructions:</h3>
+                    <ol>
+                        <li></li>
+                    </ol>
+                </section> )}
+            
         </main>
     );
 };
