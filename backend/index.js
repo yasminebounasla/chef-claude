221c20ba9f038604697c8703f7e465b4ecdfd865
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-12 07:26:29 +0200

    add the env config to the server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app  = express()
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 
@@ -7,6 +12,6 @@ app.get("/", (req, res) => {
     res.json({message :"Chef claude server is running !"})
 })
 
-app.listen(env.PORT, ()=> {
-    console.log("Server running on port 5000");
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`);
 })
