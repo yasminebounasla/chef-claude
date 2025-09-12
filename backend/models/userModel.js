ee75e488b44b7dd0649ed27fd66e9c3c5d967491
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-12 09:44:37 +0200

    fix typo error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name : String,
     email : {type: String , unique : true},
-    passwrord : String
+    passwrod : String
 } , { timestamps: true })
 
 export default mongoose.model('User', userSchema);
