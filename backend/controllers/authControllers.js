6d92ba0c88d8fd82163ab7f19b4ce4a0eed3d0a8
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-12 09:41:31 +0200

    create register endpoint

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,62 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import { validatePassword } from "../utils/validatePassword.js"; 
+
+
+export const register = async (req, res) => {
+    const { email, password, confirmPassword, name } = req.body;
+    
+    try {
+        const jwtSecret = process.env.JWT_SECRET;
+        if (!jwtSecret) {
+            return res.status(500).json({ 
+                message: "JWT_SECRET is not configured" 
+            });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        const passwordErr = validatePassword(password);
+        if (passwordErr) {
+            return res.status(400).json({ message: passwordErr });
+        }
+
+        if (password !== confirmPassword) { 
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
+
+        const existingUser = await User.findOne({
+            email: email.trim().toLowerCase()
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            email: email.trim().toLowerCase(),
+            password: hashedPassword,
+            name: name.trim()
+        });
+
+        const token = jwt.sign({ userId: user._id, name: user.name }, jwtSecret, { expiresIn: "1h" });
+        user.password = undefined;
+
+        res.status(201).json({
+            message: "User registered successfully",
+            data: { user, token }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to register user",
+            error: err.message
+        });
+    }
+};
