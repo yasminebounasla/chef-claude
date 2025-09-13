c27692988f8a765fb714f753ffc88c57b539de8c
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-13 11:06:15 +0200

    add profile , history and favorite routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { register, login } from "../controllers/authControllers.js";
+import { register, login, getProfile, editProfile, deleteProfile } from "../controllers/authControllers.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router()
 
 router.post('/register', register);
-router.post('/login', login)
+router.post('/login', login);
+
+router.get('/profile', auth, getProfile);
+router.put('/profile', auth,  editProfile);
+router.delete('/profile', auth, deleteProfile);
 
 export default router;
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,5 +1,18 @@
 import express from "express";
+import { getHistory, addToHistory, clearHistory, deleteHistory, addToFavorite, toggleFavorite, removeFavorite, getFavorites} from "../controllers/recipeControllers";
+import { auth } from "../middleware/auth";
 
 const router = express.Router()
+
+router.get('/history', auth, getHistory);
+router.post('/history', auth, addToHistory);
+router.delete('/history', auth, clearHistory)
+router.delete('/history',auth, deleteHistory);
+
+router.post('/favorite', auth, addToFavorite);
+router.delete('/favorite', auth, removeFavorite);
+router.get('/favorite', auth, getFavorites);
+router.get('/favorite', auth, toggleFavorite);
+
 
 export default router;
