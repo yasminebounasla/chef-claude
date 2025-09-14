508666a1d791ce1e1d8a7eb3bf2feb2ab00d88f2
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-14 10:26:10 +0200

    add error handlers in sepearet file

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,6 +1,36 @@
+import { AppError } from '../utils/AppError.js';
 
 export const errorHandler = (err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        message : err.message || "Something went wrong"
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Something went wrong';
+    
+    // Invalid MongoDB ID 
+    if (err.name === 'CastError') {
+        message = 'Invalid ID format';
+        statusCode = 400;
+    }
+    
+    // user already exists
+    if (err.code === 11000) {
+        message = 'This email is already registered';
+        statusCode = 400;
+    }
+    
+    // Missing required fields
+    if (err.name === 'ValidationError') {
+        message = 'Please fill in all required fields';
+        statusCode = 400;
+    }
+    
+    // Invalid or expired JWT token
+    if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+        message = 'Please log in again';
+        statusCode = 401;
+    }
+
+    // Send the error response
+    res.status(statusCode).json({
+        status: 'error',
+        message: message
     });
 };
