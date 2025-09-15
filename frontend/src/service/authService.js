5d187a0bafa2b7ed7536ff632cce6d760d3cb8e2
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-15 13:54:04 +0200

    add authService file

---
+++ frontend/src/service/authService.js
@@ -0,0 +1,15 @@
+import { authFetch, BASE_URL } from "../utils/api";
+
+export const loginUser = async (email, password) => {
+    return authFetch(`${BASE_URL}/auth/login`, {
+        method : "POST",
+        body: JSON.stringify({ email, password })
+    });
+}
+
+export const registerUser = async (email, password, confirmPassword, name) => {
+    return authFetch(`${BASE_URL}/auth/register`, {
+        method: "POST",
+        body : JSON.stringify({email, password, confirmPassword, name})
+    })
+}
