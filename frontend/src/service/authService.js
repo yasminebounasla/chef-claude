31e09ffa8bedc32c9252f9ef7c6c4a6da71bcaf1
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-15 15:09:00 +0200

    add recipeService file

--- frontend/src/service/authService.js
+++ frontend/src/service/authService.js
@@ -1,13 +1,13 @@
 import { authFetch, BASE_URL } from "../utils/api";
 
-export const loginUser = async (email, password) => {
+export const login = async (email, password) => {
     return authFetch(`${BASE_URL}/auth/login`, {
         method : "POST",
         body: JSON.stringify({ email, password })
     });
 }
 
-export const registerUser = async (email, password, confirmPassword, name) => {
+export const register = async (email, password, confirmPassword, name) => {
     return authFetch(`${BASE_URL}/auth/register`, {
         method: "POST",
         body : JSON.stringify({email, password, confirmPassword, name})
---
+++ frontend/src/service/recipeService.js
@@ -0,0 +1,49 @@
+import { authFetch, BASE_URL } from "../utils/api";
+
+export const getHistory = async () => {
+    return authFetch(`${BASE_URL}/recipe/history`, { method : "GET"}, token)
+}
+
+export const addToHistory = async (recipe, isFavorite = false) => {
+    return authFetch(`${BASE_URL}/recipe/history`, {
+        method: "POST",
+        body: JSON.stringify({
+            recipe,
+            isFavorite
+        })
+    }, token);
+}
+
+export const deleteHistory = async (recipeId) => {
+    return authFetch(`${BASE_URL}/recipe/history/${recipeId}`, {
+        method : "DELETE"
+    }, token)
+}
+
+export const clearHistory = async () => {
+    return authFetch(`${BASE_URL}/recipe/history`, {
+        method : "DELETE"
+    }, token)
+}
+
+export const addToFavorite = async (recipe, isFavorite = true) => {
+    return authFetch(`${BASE_URL}/recipe/favorites`, {
+        method: "POST",
+        body : JSON.stringify({
+            recipe,
+            isFavorite
+        })
+    }, token)
+}
+
+export const toggleFavorite = async (recipeId) => {
+    return authFetch(`${BASE_URL}/recipe/favorites/${recipeId}/toggle`, {
+        method : "PUT"
+    }, token)
+}
+
+export const removeFavorite = async (recipeId) => {
+    return authFetch(`${BASE_URL}/recipe/favorites/${recipeId}`, {
+        method : "DELETE"
+    }, token)
+}
