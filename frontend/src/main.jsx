cbd3c99e252ba413c25cc558b8ab4b89a061a2c4
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-15 18:48:44 +0200

    add the right states and handlers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './contexts/authContext.js'
+import { AuthProvider } from './contexts/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
